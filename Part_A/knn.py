188022d60f285396df1be9b77df8f39dddf80da2
Elif Erzincan <elif.erzincan@mail.utoronto.ca>
2023-03-20 23:38:40 -0400

    Completed finding k* from the validation accuracies and reporting final accuracies on test data.

--- Part_A/knn.py
+++ Part_A/knn.py
@@ -72,20 +72,41 @@ def main():
         user_accuracy = knn_impute_by_user(sparse_matrix, val_data, k)
         user_based_lst.append(user_accuracy)
         item_accuracy = knn_impute_by_item(sparse_matrix, val_data, k)
-        item_based_lst.append(user_accuracy)
+        item_based_lst.append(item_accuracy)
+    user_best = report_best_accuracy(user_based_lst)
+    k_user = user_based_lst.index(user_best)
+    item_best = report_best_accuracy(item_based_lst)
+    k_item = item_based_lst.index(item_best)
+
+    print("Highest performance of user based collaborative filtering on the validation data is " + str(user_best) +
+          "for k = " + str(k_user))
+    print("Highest performance of item based collaborative filtering on the validation data is " + str(item_best) +
+          "for k = " + str(k_item))
+    test_accuracy_user = knn_impute_by_user(sparse_matrix, test_data, k_user)
+    print("Final test accuracy of user based collaborative filtering on test data is " + str(test_accuracy_user))
+    test_accuracy_item = knn_impute_by_item(sparse_matrix, test_data, k_item)
+    print("Final test accuracy of item based collaborative filtering on test data is " + str(test_accuracy_item))
 
     plt.plot(k_lst, user_based_lst)
     plt.xlabel("Value of K")
-    plt.ylabel("Validation Accuracy")
+    plt.ylabel("Validation Accuracy for User Based Collaborative Filtering")
     plt.show()
 
     plt.plot(k_lst, item_based_lst)
     plt.xlabel("Value of K")
-    plt.ylabel("Validation Accuracy")
+    plt.ylabel("Validation Accuracy for Item Based Collaborative Filtering")
     plt.show()
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
+
+
+def report_best_accuracy(lst: list):
+    curr = 0
+    for i in lst:
+        if i >= curr:
+            curr = i
+    return curr
 
 
 if __name__ == "__main__":
